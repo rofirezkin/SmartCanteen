6280711c9bdb4c967061c6a40c268f751acab12f
muhammad fajar <invasionfajar@gmail.com>
2021-12-21 10:04:04 +0700

    Add Rating

--- src/pages/FeedbackPage/index.js
+++ src/pages/FeedbackPage/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import axios from 'axios';
+import React , {useState} from 'react';
+import {Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {ICStartActive, ICStartNonActive} from '../../assets';
 import {
   Button,
@@ -8,10 +9,106 @@ import {
   Header,
   RatingFeedback,
 } from '../../components';
+import { ENDPOINT_API_SMART_CANTEEN } from '../../utils/API/httpClient';
 
 const FeedbackPage = ({navigation, route}) => {
   const dataParams = route.params;
-  console.log('data', dataParams);
+  console.log(dataParams)
+  const [maxRating,setMaxRating] = useState([1,2,3,4,5]);
+  const [defaultRating, setDefaultRating] = useState(2);
+  const id_tenant = dataParams.id_tenant
+  
+
+  // Perhitungan Rating inputan user
+  const perhitungan = defaultRating * dataParams.quantity
+
+  // Perhitungan Akhir Database 
+  var perhitunganAkhirTotal = dataParams.tenant.perhitungan_akhir
+  if(perhitunganAkhirTotal == null)
+  {
+    perhitunganAkhirTotal = 0
+  }
+
+
+  // Perhitungan Jumlah Order
+  var totalJumlahOrder = dataParams.tenant.total_jumlah_order;
+
+  if(totalJumlahOrder == null)
+  {
+    totalJumlahOrder = 0
+  }
+
+  // ini input database
+  var pembagiJumlahOrder = dataParams.quantity + totalJumlahOrder;
+
+  // Perhitungan Akhir
+  const a = perhitunganAkhirTotal + perhitungan;
+  var b = 0;
+
+    if(totalJumlahOrder == 0)
+  {
+      b = a / dataParams.quantity;
+      
+  }if(totalJumlahOrder > 0){
+      b = a / pembagiJumlahOrder
+  }
+  
+  const data1 = {
+      rating: b
+  }
+  
+  const datab = {
+      perhitungan_akhir: a
+  }
+
+  const datac = {
+    total_jumlah_order : pembagiJumlahOrder
+  }
+
+  const onSubmit = () => {
+    axios.all([
+      axios.post(`${ENDPOINT_API_SMART_CANTEEN}tenant/rating/${id_tenant}`, data1),
+      axios.post(`${ENDPOINT_API_SMART_CANTEEN}tenant/updatePerhitunganAkhir/${id_tenant}`, datab),
+      axios.post(`${ENDPOINT_API_SMART_CANTEEN}tenant/updateTotalJumlahOrder/${id_tenant}`, datac)
+            .then(axios.spread((res1,res2,res3) => {
+              console.log('Berhasil')
+              navigation.reset({index: 0, routes: [{name: 'MainApp'}]})
+            })).catch(err => {
+              console.log(err.response)
+            })
+
+    ])
+  }
+
+
+  const starImgFilled = 'https://github.com/tranhonghan/images/blob/main/star_filled.png?raw=true'
+  const startImgCorner = 'https://raw.githubusercontent.com/tranhonghan/images/main/star_corner.png'
+
+  const CusomRatingBar = () => {
+    return(
+      <View style={styles.customRatingStyle}>
+          {maxRating.map((item, key) => {
+            return(
+                <TouchableOpacity
+                  key={item}
+                  onPress={() => setDefaultRating(item)}
+                >
+                  <Image
+                    style={styles.starImgStyle}
+                    source={
+                      item <= defaultRating
+                            ? {uri: starImgFilled}
+                            : {uri: startImgCorner}
+                    }
+                  />
+
+                </TouchableOpacity>
+              )
+          })}
+      </View>
+    )
+  }
+
   return (
     <View style={styles.page}>
       <Header
@@ -30,7 +127,12 @@ const FeedbackPage = ({navigation, route}) => {
         />
         <Gap height={20} />
         <View style={styles.rating}>
-          <RatingFeedback />
+          <CusomRatingBar />
+          <View style={{ alignItems: 'center', marginTop: 20 }}>
+            <Text style={{ fontFamily: 'Poppins-Medium', fontSize: 15  }}>
+                {defaultRating + '/' + maxRating.length}
+            </Text>
+          </View>
         </View>
         <Gap height={15} />
         {/* <TextInput
@@ -43,12 +145,11 @@ const FeedbackPage = ({navigation, route}) => {
           //   onChangeText={value => setDescription(value)}
           //   value={description}
         /> */}
+        
         <Gap height={15} />
         <Button
           label="Submit"
-          onPress={() =>
-            navigation.reset({index: 0, routes: [{name: 'MainApp'}]})
-          }
+          onPress={onSubmit}
         />
       </View>
     </View>
@@ -81,4 +182,14 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     textAlignVertical: 'top',
   },
+    customRatingStyle:{
+      justifyContent: 'center',
+      flexDirection: 'row',
+      marginTop: 10
+  },
+  starImgStyle: {
+        width: 25,
+        height: 25,
+        resizeMode: 'cover'
+    },
 });
